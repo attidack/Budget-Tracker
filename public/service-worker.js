6aa1205813efc52a76b76d9a0392daf2a3e64a61
Jon Bird <attid@Jons-Laptop>
2022-09-01 16:04:44 -0600

    fixing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,68 +17,53 @@ const FILES_TO_CACHE = [
   'js/index.js'
 ];
 
-// Install the service worker
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('installing cache : ' + CACHE_NAME)
       return cache.addAll(FILES_TO_CACHE)
     })
-  ); self.skipWaiting();
-});
+  )
+})
 
-// Cache resources
-self.addEventListener('activate', function(e) {
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
 
-  self.clients.claim();
-});
-
-// Respond with cached resources
-self.addEventListener('fetch', function(e) {
-  if (e.request.url.includes('/api/')) {
-    e.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then(cache => {
-          return fetch(e.request)
-            .then(response => {
-              if (response.status === 200) {
-                cache.put(e.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch(err => {
-              return cache.match(e.request);
-            });
-        })
-        .catch(err => console.log(err))
-    );
-
-    return;
-  }
-
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (e.request.headers.get('accept').includes('text/html')) {
-          return caches.match('/');
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
         }
-      });
+      }));
     })
   );
 });
