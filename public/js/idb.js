8ddd6b378f05fd12b51163a6a1b527f81065f6fd
Jon Bird <attid@Jons-Laptop>
2022-09-01 13:11:42 -0600

    updated idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,7 +3,7 @@ const request = indexedDB.open('pizza_hunt', 1);
 
 request.onupgradeneeded = function(event) {
   const db = event.target.result;
-  db.createObjectStore('new_pizza', { autoIncrement: true });
+  db.createObjectStore('budget_tracker', 1);
 };
 
 request.onsuccess = function(event) {
@@ -12,7 +12,7 @@ request.onsuccess = function(event) {
 
   // check if app is online, if yes run checkDatabase() function to send all local db data to api
   if (navigator.onLine) {
-    uploadPizza();
+    uploadBudget();
   }
 };
 
@@ -22,28 +22,28 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+  const transaction = db.transaction(['pending'], 'readwrite');
 
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+  const budgetObjectStore = transaction.objectStore('pending');
 
   // add record to your store with add method.
-  pizzaObjectStore.add(record);
+  budgetObjectStore.add(record);
 }
 
-function uploadPizza() {
+function uploadBudget() {
   // open a transaction on your pending db
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+  const transaction = db.transaction(['pending'], 'readwrite');
 
   // access your pending object store
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+  const ObjectStore = transaction.objectStore('pending');
 
   // get all records from store and set to a variable
-  const getAll = pizzaObjectStore.getAll();
+  const getAll = ObjectStore.getAll();
 
   getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
@@ -57,10 +57,10 @@ function uploadPizza() {
             throw new Error(serverResponse);
           }
 
-          const transaction = db.transaction(['new_pizza'], 'readwrite');
-          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const ObjectStore = transaction.objectStore('pending');
           // clear all items in your store
-          pizzaObjectStore.clear();
+          ObjectStore.clear();
         })
         .catch(err => {
           // set reference to redirect back here
@@ -71,4 +71,4 @@ function uploadPizza() {
 }
 
 // listen for app coming back online
-window.addEventListener('online', uploadPizza);
+window.addEventListener('online', uploadBudget);
